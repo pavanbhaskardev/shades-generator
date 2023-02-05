4910b1d414ded2ec4c92bb635f975d2daa178e2c
Challa Pavan Bhaskar <107108400+pavanbhaskardev@users.noreply.github.com>
2023-02-05 11:01:29 +0530

    version 1.2

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Main />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Text, VStack, HStack, Tag } from "@chakra-ui/react";
+import { FaGithub, FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
+import "../CSS/global.css";
+
+const Footer = () => {
+  return (
+    <>
+      <VStack my={4}>
+        <Text mb={2}> Made by Pavan Bhaskar with 💝</Text>
+        <HStack pb={3}>
+          <a
+            href="https://github.com/pavanbhaskardev"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaGithub className="social-media-icons" />
+          </a>
+          <a
+            href="https://linkedin.com/in/pavan-bhaskar-challa-4a2774244"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin className="social-media-icons" />
+          </a>
+
+          <a
+            href="https://twitter.com/pavanch80091410"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaTwitter className="social-media-icons" />
+          </a>
+          <a
+            href="https://www.instagram.com/pavan_bhaskar_ch/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaInstagram className="social-media-icons" />
+          </a>
+        </HStack>
+        <Tag colorScheme="messenger">Version 1.2</Tag>
+      </VStack>
+    </>
+  );
+};
+
+export default Footer;
